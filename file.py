23206f379d5cf1c300005c3ee86d8f8999cc225b
himzei <himzei@gmail.com>
2024-08-27 14:48:11 +0900

    240827 csv 파일쓰기

---
+++ file.py
@@ -0,0 +1,19 @@
+import csv 
+
+
+def save_to_csv(jobs):
+    with open("./to_save.csv", "w") as file:
+        csv_writer = csv.writer(file)
+
+        csv_writer.writerow(
+            ["No", "공고제목", "회사이름", "회사위치", "링크"]
+            )
+        
+        for index, job in enumerate(jobs):
+            csv_writer.writerow(
+                [index + 1, 
+                job["title"], 
+                job["company"], 
+                job["location"], 
+                job["link"]
+                    ])
